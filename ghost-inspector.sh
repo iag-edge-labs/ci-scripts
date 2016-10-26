c52fa27f18ce76844b20be7f381aa29412cca178
Jamie Hayman <jamie.neil.hayman@gmail.com>
2016-10-26 11:18:35 +1100

    adding ghostinspector script

--- ghost-inspector.sh
+++ ghost-inspector.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/firefox.sh | bash -s
 set -e
-if [ $(grep -c '"passing":false' ghostinspector.json) -ne 0 ];
-    then exit 1;
+if [ $(grep -c '"passing":false' ghostinspector.json) -ne 0 ]; then
+    echo 'Ghost Inspector tests failed.'
+    exit 1;
 fi
