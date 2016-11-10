b3d69dcf5b2245a2060d1cd925bcba70b495ab58
Jamie Hayman <jamie.neil.hayman@gmail.com>
2016-11-11 09:43:04 +1100

    added new build file

--- version.sh
+++ version.sh
@@ -2,6 +2,6 @@
 # curl -sSL  https://raw.githubusercontent.com/iag-edge-labs/ci-scripts/master/version.sh | bash -s
 
 # add version number to file
-APP_VERSION=`git describe --always`
+APP_VERSION=`git describe --tags`
 touch build.txt
-echo APP_VERSION > build.txt
+echo 'Release Tag: ' $APP_VERSION > build.txt
